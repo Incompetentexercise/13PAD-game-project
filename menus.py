f15d7a08dc0e1ac0dc6274a3a6f18a6d7aa4da6e
Braedon <43808091+Incompetentexercise@users.noreply.github.com>
2020-06-29 20:54:36 +1200

    base menu prototype working in surface

--- menus.py
+++ menus.py
@@ -9,14 +9,21 @@ def create_text(text, font, colour, position):
     return {'surface': _text, 'rect': _text_rect}
 
 
+def stop():
+    pygame.quit()
+
+
 class Menu:
-    def __init__(self, screen, font, resolution):
+    def __init__(self, surface, font, position):
         self.state = 'main'
-        self.screen = screen
+        self.surface = surface
+        self.rect = self.surface.get_rect()
+        self.rect.center = position
         self.font = font
         self.text_colour = (240, 240, 240)
-        self.resolution = resolution
-        self.center = (resolution[0]/2, resolution[1]/2)
+        self.resolution = surface.get_size()
+
+        self.center = (self.resolution[0]/2, self.resolution[1]/2)
 
         self.tx_play = create_text('[ENTER] - PLAY', self.font, self.text_colour, self.center)
         self.tx_instructions = create_text(
@@ -30,27 +37,51 @@ class Menu:
         elif self.state == 'instructions':
             self.do_instruction_menu()
 
+    def blit(self, surface):
+        surface.blit(self.surface, self.rect)
+
     def do_main_menu(self):
-        self.screen.fill((0, 0, 0))
-        self.screen.blit(self.tx_play['surface'], self.tx_play['rect'])
-        self.screen.blit(self.tx_instructions['surface'], self.tx_instructions['rect'])
+        self.surface.fill((0, 0, 0))
+        self.surface.blit(self.tx_play['surface'], self.tx_play['rect'])
+        self.surface.blit(self.tx_instructions['surface'], self.tx_instructions['rect'])
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:  #The user closed the window!
-                pygame.quit()  #Close the window
+                stop()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key in [pygame.K_ESCAPE]:
+                    stop()
+                elif event.key in [pygame.K_i]:
+                    self.state = 'instructions'
 
     def do_instruction_menu(self):
-        pass
+        self.surface.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  #The user closed the window!
+                stop()
+            if event.type == pygame.KEYDOWN:
+                if event.key in [pygame.K_ESCAPE]:
+                    self.state = 'main'
 
 
 if __name__ == '__main__':
     pygame.font.init()
     screen = pygame.display.set_mode((640, 480))
     clock = pygame.time.Clock()
-    menu_font = pygame.font.SysFont('Courier New', 15, False, False)
+    game_state = 'in menu'
 
-    menu = Menu(screen, menu_font, (640, 480))
+    menu_surface = pygame.Surface((200, 250))
+    menu_font = pygame.font.SysFont('Courier New', 15, False, False)
+    menu = Menu(menu_surface, menu_font, (320, 240))
+
+    screen.fill((50, 50, 50))
+    pygame.display.update()
+
     while True:
         clock.tick(30)
-        menu.do()
-        pygame.display.update()
+        if game_state == 'in menu':
+            menu.do()
+            menu.blit(screen)
+            pygame.display.update(menu.rect)
